55b7c61be1fd4c525427f11a5c7c63c4b7e95631
Vinci-da-Game <11582145@student.uts.edu.au>
2018-07-11 20:10:00 +1000

    doc: start convert xml to json

--- helpers/response/reformat.js
+++ helpers/response/reformat.js
@@ -1,5 +1,8 @@
 /*jshint esversion: 6 */
+const xmlTojson = require('xml2json');
 
 exports.RefineFlickrResult = (payload) => {
-    return payload;
+    const relay = xmlTojson.toJson(payload);
+    console.log('6 -- ', relay);
+    return relay;
 };
