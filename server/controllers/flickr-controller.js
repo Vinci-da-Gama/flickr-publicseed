a4a81765806b3fd157a744c9b4d6b37e6fd7978f
Vinci-da-Game <11582145@student.uts.edu.au>
2018-07-12 20:48:37 +1000

    fix: handle aip key invalid error.

--- server/controllers/flickr-controller.js
+++ server/controllers/flickr-controller.js
@@ -27,19 +27,27 @@ const GetFlickr = async(req, res, next) => {
 // url: https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=edc0dcfb15e1c7b098d7e0e3c19515e8&text=ice&content_type=7&format=json&per_page=20
 const SearchByText = async(req, res, next) => {
     const term = reformat.trimTerm(req.params['term']);
-    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=edc0dcfb15e1c7b098d7e0e3c19515e8&text=${term}&content_type=7&per_page=20`;
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1d75d8ab7bd0ed4e28a701f8d5e4dd73&text=${term}&content_type=7&per_page=20`;
     await axios.get(url)
         .then(resp => {
             xmlTojs(resp.data, (err, rz) => {
                 if (err) {
                     next(err);
                 } else {
-                    res.json({
-                        // return img urls collection
-                        data: reformat.RefineSearchPhotos(rz.rsp.photos[0].photo),
-                        success: true,
-                        responseCode: resCode.success
-                    });
+                    switch (rz.rsp['$'].stat) {
+                        case 'fail':
+                            const err = new Error('Api Key is invalid.');
+                            next(err);
+                            break;
+                        default:
+                            res.json({
+                                // return img urls collection -- reformat.RefineSearchPhotos(rz.rsp.photos[0].photo)
+                                data: reformat.RefineSearchPhotos(rz.rsp.photos[0].photo),
+                                success: true,
+                                responseCode: resCode.success
+                            });
+                            break;
+                    }
                 }
             });
         })
