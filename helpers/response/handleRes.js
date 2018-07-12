75fc20de8d46301ade1cfe53793df938d696ead1
Vinci-da-Game <11582145@student.uts.edu.au>
2018-07-12 22:20:14 +1000

    doc: check return data when search

--- helpers/response/handleRes.js
+++ helpers/response/handleRes.js
@@ -9,7 +9,7 @@ exports.handleRes = (req, res, result) => {
     if (term !== '') {
         res.json({
             // return img urls collection -- reformat.RefineSearchPhotos(result.rsp.photos[0].photo)
-            data: result.rsp.photos,
+            data: result,
             success: true,
             responseCode: resCode.success
         });
